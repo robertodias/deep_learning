4d3ccb0f3b289b6ebbc85b5e7b23015c7250c306
Dias, Roberto (ESI) <Roberto.Dias@ADP.com>
2018-11-15 20:08:08 -0200

    Softmax | Basic Concept for final layer of neural network perceptrons

---
+++ softmax/softmax.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+def softmax(L):
+    L_prob = []
+    L_sum = 0
+    for i in range(len(L)):
+        L_sum += math.exp(L[i])
+    for i in range(len(L)):
+        L_prob.append(math.exp(L[i])/L_sum)
+    return L_prob
